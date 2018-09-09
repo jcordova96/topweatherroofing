b542c8fe11994cb53e56803279726b786b86d3b7
Javier Cordova <jcordova96@gmail.com>
2018-09-09 09:52:35 -0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravell
                 </div>
 
                 <div class="links">
